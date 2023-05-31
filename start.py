7a2deec9e7a6404386153a6916c38cd120478a5f
pairnon <fhollis983@gmail.com>
2023-05-30 23:56:12 -0700

    added local path

--- start.py
+++ start.py
@@ -7,6 +7,7 @@ import subprocess
 ALLOC_RAM = os.getenv("ALLOC_RAM")
 JAR_FILE = os.getenv("JAR_FILE")
 RAM_SERVER_PATH = os.getenv("RAM_SERVER_PATH")
+LOCAL_WORLD_PATH = os.getenv("LOCAL_WORLD_PATH")
 
 start = "cd {ram_path} && java -Xmx{alloc_ram} -Xms{alloc_ram} -jar {jar_file} nogui".format(alloc_ram=ALLOC_RAM, jar_file=JAR_FILE, ram_path=RAM_SERVER_PATH)
 
