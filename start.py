d1ceb9b62bafcb4dce82cb65c43ad6fddbb8b2ec
pairnon <fhollis983@gmail.com>
2023-05-30 20:57:03 -0700

    fix missing jar

--- start.py
+++ start.py
@@ -8,10 +8,10 @@ ALLOC_RAM = os.getenv("ALLOC_RAM")
 JAR_FILE = os.getenv("JAR_FILE")
 SERVER_FOLDER_PATH = os.getenv("SERVER_FOLDER_PATH")
 
-subprocess.Popen("cd {path}".format(path = SERVER_FOLDER_PATH), shell=True)
-
-start = "java -Xmx{alloc_ram} -Xms{alloc_ram} -jar {jar_file} nogui".format(alloc_ram=ALLOC_RAM, jar_file=JAR_FILE)
-
-print(start)
+start = "java -Xmx{alloc_ram} -Xms{alloc_ram} -jar {path}/{jar_file} nogui".format(alloc_ram=ALLOC_RAM, jar_file=JAR_FILE, path=SERVER_FOLDER_PATH)
 
 subprocess.Popen(start, shell=True)
+
+server = Server()
+
+server.exec("/stop")
