7d0dea59c397d6ddced9375257a37a90b717684c
pairnon <fhollis983@gmail.com>
2023-05-30 21:05:17 -0700

    refactor

--- start.py
+++ start.py
@@ -6,9 +6,9 @@ import subprocess
 
 ALLOC_RAM = os.getenv("ALLOC_RAM")
 JAR_FILE = os.getenv("JAR_FILE")
-SERVER_FOLDER_PATH = os.getenv("SERVER_FOLDER_PATH")
+RAM_SERVER_PATH = os.getenv("RAM_SERVER_PATH")
 
-start = "cd {path} && java -Xmx{alloc_ram} -Xms{alloc_ram} -jar {jar_file} nogui".format(alloc_ram=ALLOC_RAM, jar_file=JAR_FILE, path=SERVER_FOLDER_PATH)
+start = "cd {ram_path} && java -Xmx{alloc_ram} -Xms{alloc_ram} -jar {jar_file} nogui".format(alloc_ram=ALLOC_RAM, jar_file=JAR_FILE, ram_path=RAM_SERVER_PATH)
 
 subprocess.Popen(start, shell=True)
 
