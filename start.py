e7068b27d670419c9b9adae583252f7243248f95
pairnon <fhollis983@gmail.com>
2023-05-30 21:00:43 -0700

    fix running in wrong dir

--- start.py
+++ start.py
@@ -8,7 +8,7 @@ ALLOC_RAM = os.getenv("ALLOC_RAM")
 JAR_FILE = os.getenv("JAR_FILE")
 SERVER_FOLDER_PATH = os.getenv("SERVER_FOLDER_PATH")
 
-start = "java -Xmx{alloc_ram} -Xms{alloc_ram} -jar {path}/{jar_file} nogui".format(alloc_ram=ALLOC_RAM, jar_file=JAR_FILE, path=SERVER_FOLDER_PATH)
+start = "cd {path} && java -Xmx{alloc_ram} -Xms{alloc_ram} -jar {jar_file} nogui".format(alloc_ram=ALLOC_RAM, jar_file=JAR_FILE, path=SERVER_FOLDER_PATH)
 
 subprocess.Popen(start, shell=True)
 
