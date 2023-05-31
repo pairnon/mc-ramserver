68c24bdf23f905e42a9c3b873c56722a7784660e
pairnon <fhollis983@gmail.com>
2023-05-30 20:50:15 -0700

    added dir change

--- start.py
+++ start.py
@@ -6,11 +6,12 @@ import subprocess
 
 ALLOC_RAM = os.getenv("ALLOC_RAM")
 JAR_FILE = os.getenv("JAR_FILE")
+SERVER_FOLDER_PATH = os.getenv("SERVER_FOLDER_PATH")
 
+subprocess.Popen("cd {path}".format(path = SERVER_FOLDER_PATH), shell=True)
 
 start = "java -Xmx{alloc_ram} -Xms{alloc_ram} -jar {jar_file} nogui".format(alloc_ram=ALLOC_RAM, jar_file=JAR_FILE)
 
 print(start)
-
 
 subprocess.Popen(start, shell=True)
